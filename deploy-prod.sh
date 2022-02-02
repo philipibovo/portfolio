b8a258790d091d23bc877b83cbbe2b126d0dccd7
Philipi Bovo <philipi@bovo.me>
2022-02-02 18:34:12 -0300

    Update Jobs (limoney pictures)

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -4,19 +4,19 @@ rm -rf dist/tivolijs
 
 ng build --configuration=production --aot=true --build-optimizer=true --project portfolio
 
-chown -R philipi: /home/philipi/Desenvolvimento/philipi/portfolio/dist
+#chown -R philipibovo: /home/philipi/Desenvolvimento/philipi/portfolio/dist
 
 mkdir dist/navonajs
 cp -r ../navonajs/* dist/navonajs
-chown -R philipi: /home/philipi/Desenvolvimento/philipi/portfolio/dist/navonajs
+#chown -R philipibovo: /Users/philipibovo/Development/philipi/portfolio/dist/navonajs
 
 mkdir dist/pompeijs
 cp -r ../pompeijs/* dist/pompeijs
-chown -R philipi: /home/philipi/Desenvolvimento/philipi/portfolio/dist/pompeijs
+#chown -R philipibovo: /Users/philipibovo/Development/philipi/portfolio/dist/pompeijs
 
 mkdir dist/tivolijs
 cp -r ../tivolijs/* dist/tivolijs
-chown -R philipi: /home/philipi/Desenvolvimento/philipi/portfolio/dist/tivolijs
+#chown -R philipibovo: /Users/philipibovo/Development/philipi/portfolio/dist/tivolijs
 
 
 
